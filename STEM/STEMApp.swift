a49cd703155680e96dc73dd568870747cefeb4b5
LauriLuigi <56127551+LauriLuigi@users.noreply.github.com>
2022-07-15 12:17:25 +0200

    Initial Commit

---
+++ STEM/STEMApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMApp.swift
+//  STEM
+//
+//  Created by Luigi Lauri on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct STEMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
